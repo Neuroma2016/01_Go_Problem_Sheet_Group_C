16dd553ba254910e75fc6b902e7a821f6118c17e
Andrius Usevas <jadrenifeni@gmail.com>
2017-09-29 13:16:53 +0100

    Simple copy/paste problems

---
+++ 01-hello-world.go
@@ -0,0 +1,11 @@
+// Hello-world program
+//SOURCE: https://tour.golang.org/welcome/1
+//Andrew Usevas
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
---
+++ 02-Current-time.go
@@ -0,0 +1,14 @@
+//Current time program
+//SOURCE: https://tour.golang.org/welcome/4
+//Andrew Usevas-group C
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Current time is", time.Now())
+}
