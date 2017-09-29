186b4ba4af62bd392ab8d08ed0087240c1795f7a
Andrius Usevas <jadrenifeni@gmail.com>
2017-09-29 13:19:26 +0100

    User input enabled

---
+++ 03-Fizz-Buzz.go
@@ -0,0 +1,31 @@
+//Fizz Buzz program
+//Andrew Usevas-group C
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var num int
+
+	fmt.Println("Please enter the number:")
+	fmt.Scanf("%d", &num)
+
+	for i := 1; i <= num; i++ {
+
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+
+}
